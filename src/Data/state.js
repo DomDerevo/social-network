caa7d121d3eec34e29f33f4638dacc09373b8aa4
DomDerevo <persik_60106@mail.ru>
2024-06-21 21:21:42 +0300

    bind

--- src/Data/state.js
+++ src/Data/state.js
@@ -1,76 +1,82 @@
+let store = {
 
 
+    _state: {
+        profilePage: {
+            postList: [
+                { message: 'Hello world', id: 1 },
+                { message: 'Я Илон Маск!', id: 2 },
+                { message: 'Полетели на марс!', id: 3 },
+                { message: 'Дорбое утро', id: 4 },
+            ],
+            newPostText: ''
+        },
+        dialoguePage: {
+            messageList: [
+                { name: 'Вася', text: 'Привет', id: 1 },
+                { name: 'Андрей', text: 'Здрасьте', id: 2 },
+                { name: 'Леха', text: 'Добрый день', id: 3 },
+            ],
+            newMessageText: ''
+        },
 
-let state = {
-    profilePage: {
-        postList: [
-            { message: 'Hello world', id: 1 },
-            { message: 'Я Илон Маск!', id: 2 },
-            { message: 'Полетели на марс!', id: 3 },
-            { message: 'Дорбое утро', id: 4 },
-        ],
-        newPostText:''
+        Navbar: {
+            friends: [
+                { img: './img/PFP1.png', name: 'Андрей', id: 1 },
+                { img: './img/PFP2.jpg', name: 'Вася', id: 2 },
+                { img: './img/PFP3.jpg', name: 'Лёха', id: 3 }
+            ]
+        },
     },
-    dialoguePage: {
-        messageList: [
-            { name: 'Вася', text: 'Привет', id: 1 },
-            { name: 'Андрей', text: 'Здрасьте', id: 2 },
-            { name: 'Леха', text: 'Добрый день', id: 3 },
-        ],
-        newMessageText:''
+
+    addPost(postText){
+        let newPost = {
+            message: postText,
+            id: this._state.profilePage.postList.length + 1,
+            likes: 0
+        }
+
+        this._state.profilePage.postList.unshift(newPost)
+        this._state.profilePage.newPostText = ''
+        this.rerenderTree(this._state)
     },
 
-    Navbar: {
-        friends: [
-            {img:'./img/PFP1.png',name:'Андрей', id: 1 },
-            {img:'./img/PFP2.jpg',name:'Вася', id: 2 },
-            {img:'./img/PFP3.jpg',name:'Лёха', id: 3 }
-        ]
-    },
-}
+    addMessage(messageText){
+        let newMessage = {
+            name: 'Артур',
+            text: messageText,
+            id: this._state.dialoguePage.messageList.length + 1,
+        }
 
-export let addPost = (postText)=>{
-    let newPost = {
-        message: postText,
-        id : state.profilePage.postList.length+1, 
-        likes:0
+        this._state.dialoguePage.messageList.unshift(newMessage)
+        this.rerenderTree(this._state)
+    },
+
+
+    onPostChange(text){
+        this._state.profilePage.newPostText = text
+        this.rerenderTree(this._state)
+    },
+
+    onMessageChange(text){
+        this._state.dialoguePage.newMessageText = text
+        this.rerenderTree(this._state)
+    },
+
+
+    rerenderTree(){
+        console.log('Fake function');
+    },
+
+    subscribe(observer){
+        this.rerenderTree = observer
+    },
+
+    getState(){
+        return this._state
     }
-
-    state.profilePage.postList.unshift(newPost)
-    state.profilePage.newPostText=''
-    rerenderTree(state)
 }
 
-export let addMessage = (messageText) =>{
-    let newMessage = {
-        name:'Артур',
-        text:messageText,
-        id:state.dialoguePage.messageList.length+1,
-    }
+export default store
 
-    state.dialoguePage.messageList.unshift(newMessage)
-    rerenderTree(state)
-}
-
-
-export let onPostChange =(text) =>{
-    state.profilePage.newPostText = text
-    rerenderTree(state)
-}
-
-export let onMessageChange=(text) =>{
-    state.dialoguePage.newMessageText = text
-    rerenderTree(state)
-}
-
-export default state
-
-let rerenderTree = ()=>{
-    console.log('Fake function');
-}
-
-export let subscribe=(observer) =>{
-    rerenderTree = observer 
-}
-
-window.state = state
+window.state = store._state
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-
-import state from './Data/state';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { addPost, addMessage, onPostChange ,onMessageChange, subscribe} from './Data/state'
+import store from './Data/state';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderTree = (state) => {
     root.render(
       <React.StrictMode>
-        <App state={state} addPost={addPost} addMessage={addMessage} onPostChange={onPostChange} onMessageChange={onMessageChange} />
+        <App state={state} addPost={store.addPost.bind(store)} addMessage={store.addMessage.bind(store)} onPostChange={store.onPostChange.bind(store)} onMessageChange={store.onMessageChange.bind(store)} />
       </React.StrictMode>
     );
   }
@@ -26,8 +24,8 @@ reportWebVitals();
 
 
 
-subscribe(rerenderTree)
-rerenderTree(state)
+store.subscribe(rerenderTree)
+rerenderTree(store.getState())
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
